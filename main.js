d37174d0d7337ca2c742f2ec0680dc9550fa715b
stellavleer <126340753+stellavleer@users.noreply.github.com>
2023-09-22 10:07:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function tabuada() {
+ for( count = 2; count <= 8; count++ ){
+ document.write("2 x "+ count + " = " + ( 2* count) + " <br/>");
+
+ }
+}
+
+tabuada();
+
+function alerta() {
+var nome = "stella" 
+window.alert( "Olá, me chamo " + nome )
+window.alert( "cuidado com esse site, não confiavel." )
+}
+
+function alterarImagem() {
+document.getElementById("s").src = "https://rotasdeviagem.com.br/wp-content/uploads/2021/12/praias-que-brilham-no-escuro.jpg"
+}
+
